e508ead385bf1561f16f7c33369266e0218ef449
ajinkya <ajinkya060@gmail.com>
2025-05-27 11:15:13 +0530

    Update terminal and telescope configurations

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -31,13 +31,3 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>:lua toggle_term(1)<CR>]], { noremap = 
 vim.keymap.set("n", "<leader>ft", function()
   toggle_term(1, "horizontal")
 end, { noremap = true, silent = true })
-
-vim.api.nvim_set_hl(0, "TermNormal", { link = "Normal" })
-vim.api.nvim_set_hl(0, "TermNormalNC", { link = "NormalNC" })
-
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "term://*toggleterm#*",
-  callback = function()
-    vim.api.nvim_win_set_option(0, "winhighlight", "")
-  end,
-})
--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -3,8 +3,20 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup {
+      highlights = {
+        Normal = {
+          guibg = "#282828",
+        },
+      },
+      shade_terminals = false,
       float_opts = {
-        border = "none", -- removes the border
+        border = "none", -- thin border. Use "solid" for a really minimal look (NVIM 0.9+)
+        winblend = 5,
+        highlights = {
+          border = "FloatBorder",
+          background = "NormalFloat",
+        },
+        title_pos = "center",
       },
     }
   end,
