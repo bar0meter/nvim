61e9eda76ee398aa5b70a2a1aa8c67af7052622b
ajinkya <ajinkya060@gmail.com>
2025-08-23 02:39:03 +0530

    ✨ Switch to gruvbox theme and enhance bufferline with LSP diagnostics

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,6 +6,12 @@ return {
     require("bufferline").setup {
       options = {
         separator_style = "slant",
+        diagnostics = "nvim_lsp", -- or "coc" / "ale"
+        diagnostics_update_in_insert = false,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match "error" and " " or level:match "warning" and " " or ""
+          return " " .. icon .. count
+        end,
       },
     }
   end,
