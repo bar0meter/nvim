fa454c2fc6ffb4f3854eef3c1735b8b926046cdc
bar0meter <ajinkya060@gmail.com>
2025-08-29 12:09:47 +0530

    transparency

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -23,6 +23,17 @@ local gruvbuddy = {
 
 local onedarkpro = {
   "olimorris/onedarkpro.nvim",
+  config = function()
+    require("onedarkpro").setup {
+      options = {
+        cursorline = false, -- Use cursorline highlighting?
+        transparency = true, -- Use a transparent background?
+        terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+        lualine_transparency = false, -- Center bar transparency?
+        highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+      },
+    }
+  end,
 }
 
 local kanagawa = {
