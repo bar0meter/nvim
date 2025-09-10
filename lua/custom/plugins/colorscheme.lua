abba998dcb97bef6526f66bc1e4a4da7ab2b8eac
bar0meter <ajinkya060@gmail.com>
2025-09-10 10:08:01 +0530

    transparency

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,87 +1,5 @@
-local gruvbuddy = {
-  "tjdevries/colorbuddy.nvim",
-  config = function()
-    require("colorbuddy").colorscheme "gruvbuddy"
-
-    local colorbuddy = require "colorbuddy"
-    local c = colorbuddy.colors
-    local s = colorbuddy.styles
-    local Color = colorbuddy.Color
-    local Group = colorbuddy.Group
-    Color.new("background", "#282c34")
-    Color.new("line_fg", "#565c64") -- Subtle line numbers
-
-    -- Set background via Normal group
-    Group.new("Normal", c.superwhite, c.background)
-
-    -- Update LineNr to use the same background
-    Group.new("LineNr", c.line_fg, c.background)
-    Group.new("CursorLineNr", c.yellow, nil, s.bold)
-    Group.new("CursorLine", nil, c.background:light(0.1))
-  end,
-}
-
-local onedarkpro = {
-  "olimorris/onedarkpro.nvim",
-  config = function()
-    require("onedarkpro").setup {
-      options = {
-        cursorline = false, -- Use cursorline highlighting?
-        transparency = false, -- Use a transparent background?
-        terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
-        lualine_transparency = false, -- Center bar transparency?
-        highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
-      },
-    }
-  end,
-}
-
 local kanagawa = {
   "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = true, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          -- Save an hlgroup with dark background and dimmed foreground
-          -- so that you can use it where your still want darker windows.
-          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-          -- Popular plugins that open floats will link to NormalFloat by default;
-          -- set their background accordingly if you wish to keep them dark and borderless
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-        }
-      end,
-      theme = "wave", -- Load "wave" theme
-      background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
-      },
-    }
-
-    -- setup must be called before loading
-    vim.cmd "colorscheme kanagawa"
-  end,
 }
 
 local gruvbox_material = {
@@ -92,118 +10,18 @@ local nightfox = { "EdenEast/nightfox.nvim" }
 
 local gruvbox = {
   "ellisonleao/gruvbox.nvim",
-  config = function()
-    require("gruvbox").setup {
-      terminal_colors = false, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = false,
-        emphasis = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = true,
-    }
-  end,
 }
 
 local tokyonight = {
   "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("tokyonight").setup {
-      style = "night",
-      transparent = true,
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "transparent", -- style for sidebars, see below
-        floats = "transparent", -- style for floating windows
-      },
-    }
-  end,
 }
 
 local catppuccin = {
   "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup {
-      flavour = "auto", -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- disables setting the background color.
-      float = {
-        transparent = true, -- enable transparent floating windows
-        solid = false, -- use solid styling for floating windows, see |winborder|
-      },
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      default_integrations = true,
-      auto_integrations = false,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
-    }
-  end,
 }
 
 return {
-  gruvbuddy,
   gruvbox_material,
-  onedarkpro,
   nightfox,
   kanagawa,
   tokyonight,
