9a871646b908bd58964c67680b84d8a040a93da4
bar0meter <ajinkya060@gmail.com>
2025-09-23 11:10:37 +0530

    updates

--- after/plugin/theme.vim
+++ after/plugin/theme.vim
@@ -1,2 +1,2 @@
-colorscheme onehalfdark
+colorscheme gruvbox
 
--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -6,11 +6,20 @@ return {
     "tpope/vim-fugitive",
     "tpope/vim-rhubarb",
     "sindrets/diffview.nvim",
+    "f-person/git-blame.nvim",
     "pwntester/octo.nvim",
     "nvim-lua/plenary.nvim",
   },
 
   config = function()
+    require("gitblame").setup {
+      enabled = true, -- if you want to enable the plugin
+      message_template = " <summary> • <author> • <<sha>> • <date>", -- template for the blame message, check the Message template section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+      message_when_not_committed = "",
+      delay = 0,
+      date_format = "%r",
+    }
     require("octo").setup {}
   end,
 }
