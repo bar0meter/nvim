e63baaf2da1a73cd291ab3a2a763f6b16402dbe6
bar0meter <ajinkya060@gmail.com>
2025-11-03 00:44:25 +0530

    disable document highlight capability

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -180,15 +180,16 @@ local lspconfig = {
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
+          client.server_capabilities.documentHighlightProvider = false
+          -- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          --   buffer = event.buf,
+          --   callback = vim.lsp.buf.document_highlight,
+          -- })
 
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
+          -- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          --   buffer = event.buf,
+          --   callback = vim.lsp.buf.clear_references,
+          -- })
         end
       end,
     })
