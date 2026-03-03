4d1973407c8fabb0cebeb44a05e137e935d38ad0
Ajinkya <ajinkya060@gmail.com>
2026-03-03 10:30:17 +0530

    🔥 refactor(config): simplify UI by removing statusline, gitsigns, and which-key
    - Remove iTerm-specific theme logic, always use vscode colorscheme
    - Remove gitsigns plugin and its gutter signs
    - Remove which-key plugin
    - Remove mini.statusline and mini.tabline setup
    - Set laststatus=0 and signcolumn=no for cleaner UI
    - Add winbar with centered file path and custom background
    - Set cmdheight=0 to hide command line when idle
    - Remove showtabline option
    
    Signed-off-by: Ajinkya <ajinkya060@gmail.com>

---
+++ after/plugin/winbar.vim
@@ -0,0 +1,4 @@
+augroup WinbarBackground
+    autocmd VimEnter * hi WinBar guibg=#D4D3D3
+    autocmd VimEnter * hi WinBar guifg=#1e1e1e gui=bold cterm=bold
+augroup END
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,14 +9,13 @@ vim.o.undofile = true
 vim.o.number = false
 vim.o.relativenumber = false
 vim.o.cursorline = false
-vim.o.showtabline = 2
 
 vim.o.completeopt = "menu,menuone,noselect,preview"
 vim.o.pumheight = 10
 vim.o.winborder = "rounded"
 vim.o.showmode = false
 
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = "no"
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.incsearch = true
@@ -37,7 +36,14 @@ vim.o.breakindent = true
 
 vim.o.colorcolumn = "0"
 
+vim.opt.cmdheight = 0
+
+-- vim.opt.winbar = "%=%{expand('%:p:h:t')}/%t%="
+vim.opt.winbar = "%=%f%="
+
 vim.diagnostic.config({ virtual_text = true })
+
+vim.o.laststatus = 0
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
