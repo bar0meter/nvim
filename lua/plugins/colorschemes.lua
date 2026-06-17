2fa33f7a23abbed091e22f94799265815886a7ea
Ajinkya <ajinkya@coderabbit.ai>
2026-06-17 14:26:04 +0530

    ✨ feat(colorschemes): switch to gruvbox with UI improvements
    Signed-off-by: Ajinkya <ajinkya@coderabbit.ai>

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,9 +3,23 @@ return {
 		GH("ajmwagar/vim-deus"),
 		GH("EdenEast/nightfox.nvim"),
 		GH("rebelot/kanagawa.nvim"),
-		GH("mofiqul/vscode.nvim")
+		GH("mofiqul/vscode.nvim"),
+		GH("ellisonleao/gruvbox.nvim")
 	},
 	setup = function()
-		vim.cmd.colorscheme("deus")
+		require("gruvbox").setup({
+			bold = false,
+			italic = {
+				strings = false,
+				emphasis = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			},
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			contrast = "hard", -- can be "hard", "soft" or empty string
+		})
+
+		vim.cmd.colorscheme("gruvbox")
 	end,
 }
