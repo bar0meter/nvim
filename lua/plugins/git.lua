4651c411e12e9cad6f5579f22f2fe813eacfc092
Ajinkya <ajinkya@coderabbit.ai>
2026-06-18 20:19:29 +0530

    ✨ feat(git): add octo.nvim plugin for GitHub integration
    Signed-off-by: Ajinkya <ajinkya@coderabbit.ai>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
 	packs = {
 		GH("lewis6991/gitsigns.nvim"),
+		GH("pwntester/octo.nvim")
 	},
 	setup = function()
 		require("gitsigns").setup({
@@ -10,6 +11,14 @@ return {
 				delete = { text = "_" },
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
+			},
+		})
+
+		require("octo").setup({
+			file_panel = {
+				icons = function(name, _ext)
+					return require("mini.icons").get("file", name)
+				end,
 			},
 		})
 	end,
