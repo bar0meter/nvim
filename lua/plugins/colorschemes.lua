6ee1937ba7943e17f05cc90ce2d66df3f84d935e
Ajinkya <ajinkya@coderabbit.ai>
2026-07-27 06:26:13 +0530

    ✨ Add more colorscheme plugins and switch to tokyonight
    Signed-off-by: Ajinkya <ajinkya@coderabbit.ai>

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -4,9 +4,12 @@ return {
     GH("EdenEast/nightfox.nvim"),
     GH("jnurmine/zenburn"),
     GH("morhetz/gruvbox"),
-    GH("navarasu/onedark.nvim")
+    GH("navarasu/onedark.nvim"),
+    GH("kepano/flexoki-neovim"),
+    GH("rose-pine/neovim"),
+    GH("folke/tokyonight.nvim")
   },
   setup = function()
-    vim.cmd.colorscheme("onedark")
+    vim.cmd.colorscheme("tokyonight")
   end,
 }
